0fa960166ab7e9f9d0bbfa357ec6aaf180e99f42
sven.karmann <sven.karmann@siticom.de>
2023-02-17 12:43:14 +0100

    Abschnitt 3, Video 40

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -10,9 +11,11 @@ public class User {
     private Integer id;
 
     @Size(min=2, message = "Name should have at least 2 characters.")
+    @JsonProperty("user_name")
     private String name;
 
     @Past(message = "Birthdate should be in the past.")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
