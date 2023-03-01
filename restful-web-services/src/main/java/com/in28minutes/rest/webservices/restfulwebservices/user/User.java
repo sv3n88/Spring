e36f5c0b142836731881a0b3c741fc67204b7e32
sven.karmann <sven.karmann@siticom.de>
2023-03-01 10:02:52 +0100

    Abschnitt 3, Video 48

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -12,16 +12,20 @@ import java.time.LocalDate;
 @Entity(name = "user_details")
 public class User {
 
+    protected User() {
+
+    }
+
     @Id
     @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "Name should have at least 2 characters.")
-    @JsonProperty("user_name")
+    //@JsonProperty("user_name")
     private String name;
 
     @Past(message = "Birthdate should be in the past.")
-    @JsonProperty("birth_date")
+    //@JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJpaResource.java
@@ -0,0 +1,65 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import com.in28minutes.rest.webservices.restfulwebservices.jpa.UserRepository;
+
+@RestController
+public class UserJpaResource {
+
+    private UserDaoService service;
+
+    private UserRepository repository;
+
+    public UserJpaResource(UserDaoService service, UserRepository repository) {
+        this.service = service;
+        this.repository = repository;
+    }
+
+    @GetMapping("/jpa/users")
+    public List<User> retrieveAllUsers() {
+
+        return repository.findAll();
+    }
+
+    @GetMapping("/jpa/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
+        Optional<User> user = repository.findById(id);
+
+        if (user.isEmpty()) {
+            throw new UserNotFoundException("id: " + id);
+        }
+
+        EntityModel<User> entityModel = EntityModel.of(user.get());
+
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+
+        return entityModel;
+    }
+
+    @DeleteMapping("/jpa/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        repository.deleteById(id);
+    }
+
+    @PostMapping("/jpa/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = repository.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
