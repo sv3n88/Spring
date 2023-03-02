b1b327a0a46057df3e39f6bf36e75c684a95a0b7
sven.karmann <sven.karmann@siticom.de>
2023-03-02 10:46:24 +0100

    Abschnitt 3, Video 49

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -3,3 +3,12 @@ values
 (10001, current_date(), 'Ranga'),
 (10002, current_date(), 'Sven'),
 (10003, current_date(), 'Test');
+
+insert into post(id, description, user_id)
+values
+(20001, 'I wand to learn AWS', 10001),
+(20002, 'I wand to learn cooking', 10001),
+(20003, 'I wand to learn DevOps', 10002),
+(20004, 'I wand to learn fitness', 10002),
+(20005, 'I wand to learn Multicloud', 10003);
+
